3fe8234db313300579f3ff6e7ce15a006fb95567
Juan Pablo Loureyro <juanpabloloureyro@gmail.com>
2021-05-24 22:19:06 -0300

    auth

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
     this.app = express();
     this.port = process.env.PORT;
     this.usuariosPath = "/api/users";
+    this.authPath = "/api/auth";
 
     //conectar a DB
     this.conectarDB();
@@ -34,6 +35,7 @@ class Server {
   }
 
   routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
     this.app.use(this.usuariosPath, require("../routes/user"));
   }
 
